7e9abc1d2d75402b97d4211d4436256f48c925f2
brian theuma <theumabrian@gmail.com>
2019-11-12 20:29:25 +0100

    config updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,32 @@
-const { src, dest, series } = require('gulp');
+const { src, dest, watch, series } = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+var replace = require('gulp-replace');
 
-function sass(cb) {
-  console.log('sass builder!');
-  cb();
+// File paths
+const files = { 
+  scssPath: 'src/scss/**/*.scss',
+  jsPath: 'src/js/**/*.js'
 }
 
-function copy(cb) {
-  console.log('copy builder!');
-  cb();
+function scssTask() {
+  return src(files.scssPath)
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(postcss([ autoprefixer(), cssnano() ]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('dist')
+  );
+}
+
+function copyTask() {
+  return src('*.html')
+    .pipe(dest('dist/'));
 }
   
-  exports.default = series(sass, copy);
+exports.default = series(scssTask, copyTask);
