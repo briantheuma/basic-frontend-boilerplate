ab89769ebe3a8da6224e721832b38c995c71f5c5
brian theuma <theumabrian@gmail.com>
2019-11-12 20:08:13 +0100

    added sass files and some basic configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
+const { src, dest, series } = require('gulp');
+
+function sass(cb) {
+  console.log('sass builder!');
+  cb();
+}
+
+function copy(cb) {
+  console.log('copy builder!');
+  cb();
+}
   
-  exports.default = defaultTask
+  exports.default = series(sass, copy);
